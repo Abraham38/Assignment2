f34dfd85ee328457542485b045836e3bb5ed1fba
Guy Blaise <gbmontbouli@live.co.uk>
2016-08-27 23:10:12 +0100

    adding new action and amending index

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -6,12 +6,27 @@ class ItemController < ApplicationController
   end
 
   def show
-  	@item=Item.find(params[:id])
+  	@item = Item.find(params[:id])	
   end
 
   def new
+  	@item=Item.new
+  end
+
+  def create
+  	@item=Item.create(item_params)
+  	if @item.save
+  		redirect_to  root_path
+  	else
+  		render "Oops! create failed, try again"
+  	end
   end
 
   def edit
+  end
+  # item_params method below defines the scope of changes(id,name & price) a user can make to the database
+  private
+  def item_params
+  	params.require(:item).permit(:id, :name, :price)
   end
 end
